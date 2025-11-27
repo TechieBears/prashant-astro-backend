3270d714a33a7fc9fc6c92c633add9406300cb50
rohitmiryala <rohitmiryala@gmail.com>
2025-11-27 11:20:43 +0530

    minor issue fixed

--- src/modules/callAstrologer/call.controller.js
+++ src/modules/callAstrologer/call.controller.js
@@ -11,7 +11,7 @@ exports.createCall = asyncHandler(async (req, res) => {
 });
 
 exports.getAllCallsCustomer = asyncHandler(async (req, res) => {
-    const {page = 1, limit = 10} = req.body;
+    const {page = 1, limit = 10} = req.query;
     const skip = (page - 1) * limit;
     const total = await CallAstrologer.countDocuments({ userId: req.user._id });
     const calls = await CallAstrologer.find({ userId: req.user._id })
