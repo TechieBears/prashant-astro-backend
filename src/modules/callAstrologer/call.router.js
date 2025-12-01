b0f73e684b15a001217d0cf335cb8f5bc085e01b
rohitmiryala <rohitmiryala@gmail.com>
2025-12-01 11:28:52 +0530

    minor change

--- src/modules/callAstrologer/call.router.js
+++ src/modules/callAstrologer/call.router.js
@@ -5,7 +5,7 @@ const { protect, authorize } = require('../../middlewares/auth');
 const router = express.Router();
 
 router.post('/create', protect, authorize("customer"), CallController.createCall);
-router.get('/get-all', protect, authorize("admin", "employee"), CallController.getAllCalls);
+router.get('/get-all', protect, authorize("admin", "customer"), CallController.getAllCalls);
 router.get('/public/get-all',protect, authorize("customer"), CallController.getAllCallsCustomer);
 
 module.exports = router;
