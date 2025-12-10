152669a0c38625183bbf32e83379a2ea0e45c5c7
rohitmiryala <rohitmiryala@gmail.com>
2025-12-10 12:33:38 +0530

    employee register minor change

--- src/modules/employeeUser/employeeUser.controller.js
+++ src/modules/employeeUser/employeeUser.controller.js
@@ -54,7 +54,7 @@ exports.createEmployeeUser = asyncHandler(async (req, res, next) => {
     for (const key in req.body) {
         parsedBody[key] = parseField(req.body[key]);
     }
-    const { firstName, lastName, email, mobileNo, employeeType, skills, languages, experience, startTime, endTime, days, preBooking, about, priceCharge } = parsedBody;
+    const { firstName, lastName, email, mobileNo, userType, employeeType, skills, languages, experience, startTime, endTime, days, preBooking, about, priceCharge } = parsedBody;
 
 
     // validate fields with for loop
@@ -117,7 +117,7 @@ exports.createEmployeeUser = asyncHandler(async (req, res, next) => {
         password: tempPassword,
         profileImage,
         mobileNo,
-        role: "employee",
+        role: userType? userType : "employee",
         profile: employeeProfile._id,
         isActive: true
     });
