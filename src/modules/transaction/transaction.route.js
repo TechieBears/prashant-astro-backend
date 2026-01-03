0a257ef4c3e0199b5d2eca15cf3e0e8b4eddc41a
rohitmiryala <rohitmiryala@gmail.com>
2026-01-03 13:54:59 +0530

    minor change

--- src/modules/transaction/transaction.route.js
+++ src/modules/transaction/transaction.route.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const TransactionController = require('./transaction.controller');
-
+const { protect, authorize } = require('../../middlewares/auth');
 const router = express.Router();
 
-router.get('/get-all', TransactionController.getAllTransactions);
+router.get('/get-all', protect, authorize("customer"), TransactionController.getAllTransactions);
 
 module.exports = router;
